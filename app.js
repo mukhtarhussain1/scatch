071fa65e6fbf02013cf2b33fa5dd6d24acf7a999
Mukhtar Hussain <mukhtar.hussai9@gmail.com>
2024-09-30 03:44:45 +0500

    created separate mongoose connection

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const productModel = require("./models/product-model");
 
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const ownersRouter = require("./routes/ownersRouter");
+const usersRouter = require("./routes/usersRouter");
+const productsRouter = require("./routes/productsRouter");
+
+const db = require("./config/mongoose-connection");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,8 +18,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 app.listen(3000);

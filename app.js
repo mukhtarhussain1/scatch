5448f42e217cd43514aba2a0e6d1900921b2cd0b
Mukhtar Hussain <mukhtar.hussai9@gmail.com>
2024-10-02 22:35:09 +0500

    update app file

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 const express = require("express");
 const app = express();
 
+var cors = require("cors");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const expressSession = require("express-session");
+const flash = require("connect-flash");
+
+require("dotenv").config();
+
+const indexRouter = require("./routes/index");
 const ownersRouter = require("./routes/ownersRouter");
 const usersRouter = require("./routes/usersRouter");
 const productsRouter = require("./routes/productsRouter");
 
 const db = require("./config/mongoose-connection");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+  })
+);
+app.use(flash());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
+app.use("/uploads", express.static("public/uploads"));
 
+app.use("/", indexRouter);
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
 
-app.listen(3000);
+app.listen(5000);

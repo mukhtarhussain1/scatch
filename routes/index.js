12715c33c4ab8a22021168321e23e9ff8406bfa4
Mukhtar Hussain <mukhtar.hussai9@gmail.com>
2024-10-02 22:34:30 +0500

    create routes

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const isloggedin = require("../middlewares/isLoggedIn");
+const productModel = require("../models/product-model");
+const userModel = require("../models/user-model");
+
+router.get("/", function (req, res) {
+  let error = req.flash("error");
+  res.render("index", { error, loggedin: false });
+});
+
+router.get("/shop", isloggedin, async function (req, res) {
+  let products = await productModel.find();
+  let success = req.flash("success");
+  res.render("shop", { products, success });
+});
+
+router.get("/cart", isloggedin, async function (req, res) {
+  let user = await userModel
+    .findOne({ email: req.user.email })
+    .populate("cart");
+
+  const bill = Number(user.cart[0].price) + 20 - Number(user.cart[0].discount);
+
+  res.render("cart", { user, bill });
+});
+
+router.get("/addtocart/:productid", isloggedin, async function (req, res) {
+  let user = await userModel.findOne({ email: req.user.email });
+  user.cart.push(req.params.productid);
+  await user.save();
+  req.flash("success", "Added to cart");
+  res.redirect("/shop");
+});
+
+module.exports = router;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const {
+  registerUser,
+  loginUser,
+  fetchUserData,
+  logout,
+  updateProfile
+} = require("../controllers/authController");
+const upload = require("../config/multer-config");
+
 
 router.get("/", (req, res) => {
-    res.send("Home Page");
-  });
+  res.send("Home Page");
+});
+
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
+router.get("/logout", logout);
+
+router.get('/fetchUserData', fetchUserData);
+
+router.put('/updateProfile', upload.single('profilePicture'), updateProfile);
 
 module.exports = router;
-
